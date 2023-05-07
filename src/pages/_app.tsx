bea124b69fa831a1e0c4308954fc6cc97295093b
GiorgosMarga <giorgosmarga509@gmail.com>
2023-05-07 23:12:04 +0300

    added loading bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,18 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { RecoilRoot } from "recoil";
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
+const progress = new ProgressBar({
+  size: 4,
+  color: "#0096ff",
+  className: "z-50 ",
+  delay: 80,
+});
 
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <RecoilRoot><Component {...pageProps} /></RecoilRoot>;
 };
